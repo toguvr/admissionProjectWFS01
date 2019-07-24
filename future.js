917a170134bfd55c8fab4752b726ef3e38fa2bde
toguvr <augustotf93@gmail.com>
2019-07-24 00:31:22 -0300

    criado um arquivo externo apenas para o js, email ja sendo validado e começando a gerar o bem vindo com o email digitado.

---
+++ future.js
@@ -0,0 +1,26 @@
+
+var clique = document.querySelector('input.button');
+var textoDigitado = document.querySelector('input[name=email]');
+
+clique.onclick = function textoEmail(){
+    if( document.forms[0].email.value==" " 
+    || document.forms[0].email.value.indexOf('@')==-1 
+    || document.forms[0].email.value.indexOf('.')==-1 )
+    {
+            alert( "Por favor, informe um E-MAIL valido!" );     
+    }else{
+        var email = textoDigitado.value;
+        alert('O email ' + email + ' foi enviado');
+
+        var elemento = document.createElement('h1');
+        var criaTexto = document.createTextNode('Bem Vindo, ');
+        elemento.appendChild(criaTexto);
+        var local = document.querySelector('#msg');
+        local.appendChild(elemento);
+            
+       
+         }   
+}
+
+
+
