b992c5327004f94e24d5b7400f4a7bbde9f444af
toguvr <augustotf93@gmail.com>
2019-07-24 10:32:48 -0300

    retorno de bem vindo apos email ser validado e alerta acionado, funcionando.

--- future.js
+++ future.js
@@ -8,15 +8,18 @@ clique.onclick = function textoEmail(){
     || document.forms[0].email.value.indexOf('.')==-1 )
     {
             alert( "Por favor, informe um E-MAIL valido!" );     
+
+            return false;
     }else{
         var email = textoDigitado.value;
         alert('O email ' + email + ' foi enviado');
 
         var elemento = document.createElement('h1');
-        var criaTexto = document.createTextNode('Bem Vindo, ');
+        var criaTexto = document.createTextNode('Bem Vindo, '+ email);
         elemento.appendChild(criaTexto);
         var local = document.querySelector('#msg');
         local.appendChild(elemento);
+        return false;
             
        
          }   
